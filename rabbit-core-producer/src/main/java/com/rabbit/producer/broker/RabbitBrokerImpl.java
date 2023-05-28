10f85aea99c9cd74732980bfe09163f93514789c
沈荣 <736644385@qq.com>
2023-05-28 16:55:30 +0800

    确认消息实现

--- rabbit-core-producer/src/main/java/com/rabbit/producer/broker/RabbitBrokerImpl.java
+++ rabbit-core-producer/src/main/java/com/rabbit/producer/broker/RabbitBrokerImpl.java
@@ -31,7 +31,8 @@ public class RabbitBrokerImpl implements RabbitBroker {
 
     @Override
     public void confirmSend(Message message) {
-
+        message.setMessageType(MessageType.CONFIRM);
+        sendKernel(message);
     }
 
     @Override
